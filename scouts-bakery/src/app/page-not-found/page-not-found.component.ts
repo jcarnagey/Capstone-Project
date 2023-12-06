6adba80052cb062aabe3415e7083c258dbe3ef02
John <johncarnagey14@gmail.com>
2023-12-06 12:17:20 -0600

    updated

--- scouts-bakery/src/app/page-not-found/page-not-found.component.ts
+++ scouts-bakery/src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent  implements OnInit{
+  @Input()
+  visible = false;
+  @Input()
+  notFoundMessage = "Nothing Found!";
+  @Input()
+  resetLinkText = "Reset";
+  @Input()
+  resetLinkRoute = "/";
+  constructor() { }
 
+  ngOnInit(): void {
+      
+  }
 }
